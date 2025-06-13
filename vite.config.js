d395cb058d657022b4356a07e30ec92bd2a68d56
Sukesh Reddy Rondla <65500920+SukeshRondla@users.noreply.github.com>
2025-06-12 22:25:59 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 function App() {
   // ...
   return (
-    <Router basename="/my-portfolio">
+    <Router basename="/">
       {/* ... */}
     </Router>
   );
